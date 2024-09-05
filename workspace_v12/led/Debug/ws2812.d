d7514778e7c0ab66fd22b6795d4e074e8bf582ae
175Samantha175 <12samanthase34@gmail.com>
2024-09-05 16:45:22 -0400

    commit

---
+++ workspace_v12/led/Debug/ws2812.d
@@ -0,0 +1,24 @@
+# FIXED
+
+ws2812.obj: ../ws2812.c
+ws2812.obj: C:/ti/ccs1280/ccs/ccs_base/msp430/include/msp430.h
+ws2812.obj: C:/ti/ccs1280/ccs/ccs_base/msp430/include/msp430g2233.h
+ws2812.obj: C:/ti/ccs1280/ccs/ccs_base/msp430/include/in430.h
+ws2812.obj: C:/ti/ccs1280/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+ws2812.obj: C:/ti/ccs1280/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+ws2812.obj: ../ws2812.h
+
+../ws2812.c:
+
+C:/ti/ccs1280/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1280/ccs/ccs_base/msp430/include/msp430g2233.h:
+
+C:/ti/ccs1280/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1280/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs1280/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+../ws2812.h:
+
---
+++ workspace_v12/msp430g2xx3_usci_uart_standard_transceiver.c/Debug/subdir_vars.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+C_SRCS += \
+../msp430g2xx3_usci_uart_standard_transceiver.c \
+../ws2812.c 
+
+C_DEPS += \
+./msp430g2xx3_usci_uart_standard_transceiver.d \
+./ws2812.d 
+
+OBJS += \
+./msp430g2xx3_usci_uart_standard_transceiver.obj \
+./ws2812.obj 
+
+OBJS__QUOTED += \
+"msp430g2xx3_usci_uart_standard_transceiver.obj" \
+"ws2812.obj" 
+
+C_DEPS__QUOTED += \
+"msp430g2xx3_usci_uart_standard_transceiver.d" \
+"ws2812.d" 
+
+C_SRCS__QUOTED += \
+"../msp430g2xx3_usci_uart_standard_transceiver.c" \
+"../ws2812.c" 
+
+
