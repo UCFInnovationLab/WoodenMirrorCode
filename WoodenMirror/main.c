fc4913a7b9ee1f82e9a9b19fa1b894d299255bb4
175Samantha175 <12samanthase34@gmail.com>
2024-09-09 15:12:23 -0400

    added timer

--- WoodenMirror/main.c
+++ WoodenMirror/main.c
@@ -19,10 +19,12 @@
 
 #define UART_MODE       SMCLK_115200
 
-#define servo_min       1000
-#define servo_max       2000
+#define RESET_TIMEOUT 32768     // Timeout value for resetting (1 second at 32kHz ACLK)
 
-volatile bool reset = true;
+
+volatile uint8_t reset = 1;
+volatile uint8_t stored_byte = 0; // Store the received byte
+volatile uint8_t i = 0;
 
 void initUART()
 {
@@ -86,16 +88,28 @@ void initGPIO()
     P2OUT &= ~BIT0;             // Set P2.0 to low (0V) to turn on the LED
 }
 
+
 void initServo()
 {
     TA0CCTL1  = OUTMOD_7;          // Reset/set
-    TA0CCR0   = 65535;             // Period 20000
+    TA0CCR0   = 65535;             // Period 20000, timer period
     TA0CCR1   = 4000;              // Duty-cycle 2000 (2 ms)
-    TA0CTL    = (TASSEL_2 | MC_1 | ID_2); // SMCLK, timer in up-mode
+    TA0CTL    = (TASSEL_2 | MC_1 | ID_2); // SMCLK, timer in up-mode, divide by 4
+
+    TA0CCTL0 = CCIE;               // timer interrupt
 
     P1DIR    |=  0x40;             // Set P1.6 to output-direction
     P1SEL    |=  0x40;             // Set selection register 1 for timer-function
     P1SEL2   &= ~0x40;             // Clear selection register 2 for timer-function (not needed, as it is 0 after startup)
+}
+
+void initTimerA1()
+{
+    TA1CCTL1 = OUTMOD_7;
+    TA1CCR0 = 2000;
+    TA1CCR1 = 1000;
+    TA1CTL = (TASSEL_2 | MC_1 | ID_3);
+
 }
 
 void UART_SendChar(char data) {
@@ -129,6 +143,13 @@ void moveServo(uint8_t data)
     TA0CCR1 = servoPos;
 }
 
+void delay(void)
+{
+    volatile unsigned int i;
+    for (i = 10000000; i > 0; i--);  // Adjust value for desired blink rate
+}
+
+
 void gradualFill(u_int n, u_char r, u_char g, u_char b){
     int i;
     for (i = 0; i < n; i++){        // n is number of LEDs
@@ -146,26 +167,51 @@ void main()
 {
 //    WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
     WDTCTL    = WDTPW | WDTHOLD;   // Stop watchdog timer
+
     initClock();
     initGPIO();
     initUART();
+    initTimerA1();
     initServo();
     initStrip();
-    fillStrip(0xFF, 0x00, 0x00);
+    fillStrip(0x00, 0xFF, 0x00);
 
-//    P1OUT |= BIT0;                            // Turn on LED1 (P1.0)
 
   __bis_SR_register(GIE);       // Since SMCLK is source, enter LPM0, interrupts enabled
 
 
   while( 1 )                     // Endless-loop (main-program)
   {
-      gradualFill(NUM_LEDS, 0x00, 0xFF, 0x00);  // green
-      gradualFill(NUM_LEDS, 0x00, 0x00, 0xFF);  // blue
-      gradualFill(NUM_LEDS, 0xFF, 0x00, 0xFF);  // magenta
-      gradualFill(NUM_LEDS, 0xFF, 0xFF, 0x00);  // yellow
-      gradualFill(NUM_LEDS, 0x00, 0xFF, 0xFF);  // cyan
-      gradualFill(NUM_LEDS, 0xFF, 0x00, 0x00);  // red
+//      gradualFill(NUM_LEDS, 0x00, 0xFF, 0x00);  // green
+//      gradualFill(NUM_LEDS, 0x00, 0x00, 0xFF);  // blue
+//      gradualFill(NUM_LEDS, 0xFF, 0x00, 0xFF);  // magenta
+//      gradualFill(NUM_LEDS, 0xFF, 0xFF, 0x00);  // yellow
+//      gradualFill(NUM_LEDS, 0x00, 0xFF, 0xFF);  // cyan
+//      gradualFill(NUM_LEDS, 0xFF, 0x00, 0x00);  // red
+
+
+      int timer = 0;
+      int timer2 = 0;
+
+      timer = TA1R;
+
+      int i;
+      for (i = 0; i < 20000; i++);
+
+      timer2 = TA1R;
+      if (timer > 65000 && timer2 > 65000)
+      {
+          TA1R = 0;                       // Reset Timer A1 counter
+
+          int i = 0;
+          for (i = 0; i < 1000000; i++)
+          {
+              P2OUT ^= BIT0;          // Toggle P2.0 (LED on/off)
+              delay();
+          }
+      }
+
+
   }
 
 }
@@ -185,11 +231,40 @@ void __attribute__ ((interrupt(USCIAB0RX_VECTOR))) USCI0RX_ISR (void)
 {
     if (IFG2 & UCA0RXIFG)
     {
-        uint8_t rx_val = UCA0RXBUF; // Must read UCxxRXBUF to clear the flag
-        UART_SendChar(rx_val);               // Echo received data
-        moveServo(rx_val);
 
-        __enable_interrupt();             // Re-enable global interrupts
+        uint8_t rx_val = UCA0RXBUF;     // Read the received byte (clears interrupt flag)
+
+        if (reset == 1)
+        {
+            stored_byte = rx_val;           // Store received byte
+            moveServo(stored_byte);              // Move the servo to the new position
+
+
+            reset = 0;
+        }
+        else
+        {
+
+            UART_SendChar((char)rx_val);
+
+        }
+//
+//        // Reset the timer for handling timeouts if needed
+//        TA0R = 0;                       // Reset Timer A counter
+//
+//        reset = 1;                      // Set reset flag to indicate a new byte was received
+//
+//        if (reset == 1)
+//        {
+//                // If reset flag is set, reset the servo position after timeout
+//                TA0CCR1 = 4000;  // Reset servo to original position (mid-point)
+//
+//                reset = 0;       // Clear the reset flag
+//         }
+//
+//        __enable_interrupt();             // Re-enable global interrupts
 
     }
 }
+
+
