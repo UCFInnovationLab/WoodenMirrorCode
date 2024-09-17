3b1005772ec717d1b2bc5036124b4340209e8f0b
175Samantha175 <12samanthase34@gmail.com>
2024-09-17 09:42:17 -0400

    push

--- WoodenMirror/main.c
+++ WoodenMirror/main.c
@@ -89,6 +89,12 @@ void initGPIO()
     P2DIR |= BIT0;              // Set P2.0 as output
     P2OUT &= ~BIT0;             // Set P2.0 to low (0V) to turn on the LED
 
+    P1DIR |= BIT4;
+    P1OUT &= ~BIT4;
+
+    P2DIR |= BIT4;
+    P2OUT &= ~BIT4;
+
     P2DIR |= BIT1;
     P2OUT &= ~BIT1;
 }
@@ -99,9 +105,8 @@ void initServo()
     TA0CCTL1  = OUTMOD_7;          // Reset/set
     TA0CCR0   = 65535;             // Period 20000, timer period
     TA0CCR1   = 4000;              // Duty-cycle 2000 (2 ms)
-    TA0CTL    = (TASSEL_2 | MC_1 | ID_2); // SMCLK, timer in up-mode, divide by 4
+    TA0CTL    = (TASSEL_2 | MC_3 | ID_2); // SMCLK, timer in up-mode, divide by 4
 
-    TA0CCTL0 = CCIE;               // timer interrupt
 
     P1DIR    |=  0x40;             // Set P1.6 to output-direction
     P1SEL    |=  0x40;             // Set selection register 1 for timer-function
@@ -188,51 +193,51 @@ void main()
      __bis_SR_register(GIE);       // interrupt enabled
 
 
+
   while(1)                     // Endless-loop (main-program)
   {
 
+      timer = TA1R;
 
 //      gradualFill(NUM_LEDS, 0x00, 0xFF, 0x00);  // green
 //      gradualFill(NUM_LEDS, 0x00, 0x00, 0xFF);  // blue
 //      gradualFill(NUM_LEDS, 0xFF, 0x00, 0xFF);  // magenta
-//      gradualFill(NUM_LEDS, 0xFF, 0xFF, 0x00);  // yellow
+//      gradualFill(NUM_LEDS, 0x17, 0x17, 0x00);  // yellow
 //      gradualFill(NUM_LEDS, 0x00, 0xFF, 0xFF);  // cyan
 //      gradualFill(NUM_LEDS, 0xFF, 0x00, 0x00);  // red
 
 
+//      P2OUT ^= BIT1; //toggle led
+//      __delay_cycles(30000);p1out
 
-      timer = TA1R;
-
-      P2OUT ^= BIT1;
-      __delay_cycles(65535);
 
 //      P2OUT |= BIT1; //led on
 //      delay();
-//      P2OUT &= ~BIT1; //led off
 //      delay();
+
+
+
 
       //when timer is reset and another byte is sent, that is sent to next board.
 
-//      if (timer > 16000)    //if timer greater than one millisecond, reset timer
-//      {
-//          TA1R = 0;                       // Reset Timer A1 counter
-//
-//
-//
-//          int i = 0;
-//          for (i = 0; i < 1000000; i++)
-//          {
-//              P2OUT ^= BIT0;          // Toggle P2.0 (LED on/off)
-////              delay();
-//          }
-//          fillStrip(0xFF,0x00,0x00);
-//
-//      } //else goes to interrupt
-//      else
-//      {
-//          P2OUT &= ~BIT0;          // Toggle P2.0 (LED on/off)
-//          fillStrip(0x00, 0xFF, 0x00);
-//      }
+      if (timer > 4000)    //if timer greater than one millisecond, reset timer
+      {
+
+          timer = TA1R;
+          TA1R = 0;
+
+          reset = 1; //true
+          P1OUT = BIT4;
+
+//          P2OUT &= ~BIT4; //toggle led
+          P2OUT = BIT0;
+
+
+      } //else goes to interrupt
+      else
+      {
+          P2OUT &= ~BIT0;          // Toggle P2.0 (LED on/off)
+      }
 
 
   }
@@ -254,20 +259,29 @@ void __attribute__ ((interrupt(USCIAB0RX_VECTOR))) USCI0RX_ISR (void)
 {
     if (IFG2 & UCA0RXIFG)
     {
+        P2OUT ^= BIT4;
+
 
         uint8_t rx_val = UCA0RXBUF;     // Read the received byte (clears interrupt flag)
 
-        if (TA1R == 0)                  //if timer is reset
+        if (reset == 1)                  //if timer is reset
         {
             stored_byte = rx_val;           // Store received byte
             moveServo(stored_byte);              // Move the servo to the new position
 
+//            P2OUT = BIT4;
+
+            reset = 0;
+            P1OUT &= ~BIT4;
+
         }
         else
         {
-            UART_SendChar((char)rx_val);
+//            UART_SendChar((char)rx_val);
 
         }
+        TA1R = 0;
+
 
 //        int i = 0;
 //        for (i = 0; i < 1000000; i++)
@@ -279,7 +293,7 @@ void __attribute__ ((interrupt(USCIAB0RX_VECTOR))) USCI0RX_ISR (void)
 
     }
 
-    __bic_SR_register_on_exit(GIE);
+//    __bic_SR_register_on_exit(GIE);
 }
 
 
